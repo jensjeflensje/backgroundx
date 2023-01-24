8c209d1f43f5fb29d6f68380d0ad2fe5143d7e63
Jens de Ruiter <jensgamesnl@gmail.com>
2023-01-24 19:33:01 +0100

    Initial Commit

---
+++ backgroundx/backgroundxApp.swift
@@ -0,0 +1,17 @@
+//
+//  backgroundxApp.swift
+//  backgroundx
+//
+//  Created by Jens de Ruiter on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct backgroundxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
