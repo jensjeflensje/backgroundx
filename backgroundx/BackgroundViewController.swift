c12fcb42f05c8633849b06eb238827f95b1d217c
Jens de Ruiter <jensgamesnl@gmail.com>
2023-01-25 14:56:55 +0100

    Initial commit 🎉

---
+++ backgroundx/BackgroundViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundViewController.swift
+//  backgroundx
+//
+//  Created by Jens de Ruiter on 25/01/2023.
+//
+
+import Cocoa
+
+class BackgroundViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ backgroundx/BackgroundWindow.swift
@@ -0,0 +1,12 @@
+//
+//  BackgroundWindow.swift
+//  backgroundx
+//
+//  Created by Jens de Ruiter on 24/01/2023.
+//
+
+import Cocoa
+
+class BackgroundWindow: NSObject, NSApplicationDelegate, DebugHelper {
+
+}
